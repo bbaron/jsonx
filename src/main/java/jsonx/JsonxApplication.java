28095298129fd7600ff761f42e9c0866c828d19b
Bruce Baron <baron.br@gmail.com>
2015-12-15 13:45:22 -0500

    v0

---
+++ src/main/java/jsonx/JsonxApplication.java
@@ -0,0 +1,12 @@
+package jsonx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonxApplication.class, args);
+    }
+}
---
+++ src/test/java/jsonx/JsonxApplicationTests.java
@@ -0,0 +1,18 @@
+package jsonx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JsonxApplication.class)
+@WebAppConfiguration
+public class JsonxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
